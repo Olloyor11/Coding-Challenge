11cc288028bc34d19629cd2b3b155be79020c6f8
Ollayor <s.olloyors@gmail.com>
2026-01-20 23:30:56 +0100

    Add merge sorted linked lists solution and reorganize package structure

---
+++ src/main/java/amigoscodeDataStructureProblems/ReserveStringStack.java
@@ -0,0 +1,25 @@
+package com.leetcode.easy;
+
+import java.util.Stack;
+
+public class ReserveStringStack {
+    public String reverse(String str) {
+        // 1. Create a stack
+        Stack<Character> stringStack = new Stack<>();
+
+        // 2. Push all characters onto the stack
+        //    (loop through the string)
+        for (char c : str.toCharArray()) {
+            stringStack.push(c);
+        }
+        // 3. Pop all characters and build the reversed string
+        //    (loop while stack is not empty)
+        StringBuilder reversed = new StringBuilder();
+        while (!stringStack.isEmpty()) {
+            reversed.append(stringStack.pop());
+
+        }
+        // 4. Return the reversed string
+        return reversed.toString();
+    }
+}
